7011ab1891f78a4f5944ed28c08428dd739a42cc
Jennifer Godinez <jennifer.p1210@gmail.com>
2017-10-03 09:55:58 -0700

    Added compose tweet fragment.

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -1,13 +1,16 @@
 package com.codepath.apps.restclienttemplate.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.fragments.TweetFragment;
 import com.codepath.apps.restclienttemplate.fragments.TweetsListFragment;
 
-public class TimelineActivity extends AppCompatActivity {
+public class TimelineActivity extends AppCompatActivity implements TweetFragment.ComposeTweetListener  {
 
     TweetsListFragment fragmentTweetsList;
 
@@ -25,6 +28,29 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_tweet, menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                return true;
+
+            case R.id.twit:
+                FragmentManager fm = getSupportFragmentManager();
+                TweetFragment tweetFragment = TweetFragment.newInstance("Some Title");
+                tweetFragment.show(fm, "fragment_tweet");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onPassTweetMsg(String s) {
     }
 
 }
