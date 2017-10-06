74499b8863cf9db2c97b6f2cf39bb0e571b85038
Jennifer Godinez <jennifer.p1210@gmail.com>
2017-10-05 21:21:26 -0700

    UI changes

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ShowProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ShowProfileActivity.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.TwitterApp;
-import com.codepath.apps.restclienttemplate.network.TwitterClient;
 import com.codepath.apps.restclienttemplate.fragments.ProfileTweetsFragment;
 import com.codepath.apps.restclienttemplate.models.User;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
 
 import org.parceler.Parcels;
 
@@ -40,13 +42,15 @@ public class ShowProfileActivity extends AppCompatActivity {
         tvUserName.setText(String.format("@" + user.screenName));
 
         TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
-        tvFollowing.setText(String.format(user.countFollowing + " Following")); //TODO use %1$
+        Spanned sp = Html.fromHtml("<b>" +user.countFollowing +"</b>"+" Following");
+        tvFollowing.setText(sp);
 
         TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
-        tvFollowers.setText(user.countFollowers + " Followers");
+        sp = Html.fromHtml("<b>" +user.countFollowers +"</b>"+" Followers");
+        tvFollowers.setText(sp);
 
         ImageView ivProfileImg = (ImageView) findViewById(R.id.ivProfileImg);
-        Glide.with(this).load(Uri.parse(user.profileImageUrl)).into(ivProfileImg);
+        Glide.with(this).load(Uri.parse(user.profileImageUrl.replace("_normal", ""))).into(ivProfileImg);
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ProfileTweetsFragment fragmentDemo = ProfileTweetsFragment.newInstance(user);
